f1d0adeb39045d32aa4724b1b85f72ff238148ff
pavloit <dba.devop@gmail.com>
2023-09-25 15:57:09 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+let markup = "";
+
+for (const ingredient of ingredients) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+  listItem.textContent = ingredient;
+  markup += listItem.outerHTML; 
+};
+
+list.insertAdjacentHTML('beforeend', markup);
