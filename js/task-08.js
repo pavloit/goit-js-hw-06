4abb5f2045813fe9e58eb2fdcbc8ba707db07ef4
pavloit <dba.devop@gmail.com>
2023-09-26 17:18:20 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onSubmit);
 
+function onSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements
+    
+    if (!email.value || !password.value) {
+        alert('Потрібно заповнити всі поля')
+        return
+    }
+    
+    const formData = {
+        [email.name]: email.value,
+        [password.name]: password.value  
+    };
+
+    console.log(formData);
+    event.currentTarget.reset();
+}
