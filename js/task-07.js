a3aae2aef762c1ed0d83a0e3782ac58c60976eea
pavloit <dba.devop@gmail.com>
2023-09-27 12:12:54 +0300

    fixed task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const inputRange = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 inputRange.addEventListener('input', onChange);
+text.style.fontSize = `${inputRange.value}px`;
 function onChange(event) {
     text.style.fontSize = `${event.currentTarget.value}px`;
 }
